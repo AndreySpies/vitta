5ab64275bbad4df3398218314f04294a2d279e94
Andrey Spies <andreyspies@gmail.com>
2019-02-25 16:51:09 -0300

    Created user model

---
+++ db/migrate/20190225194954_add_informations_to_users.rb
@@ -0,0 +1,10 @@
+class AddInformationsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :phone, :string
+    add_column :users, :profile_picture, :string
+    add_column :users, :rg, :string
+    add_column :users, :birth_date, :string
+  end
+end
