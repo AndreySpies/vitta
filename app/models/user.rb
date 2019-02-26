29b67090acaaae2d2bdb86e46ddd69b642912004
Andrey Spies <andreyspies@gmail.com>
2019-02-26 16:11:10 -0300

    Configured Cloundinary and add translation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  mount_uploader :profile_picture, PhotoUploader
   has_one :doctor, dependent: :destroy
   has_many :consultations, class_name: 'Consultation', foreign_key: 'patient_id'
   devise :database_authenticatable, :registerable,
