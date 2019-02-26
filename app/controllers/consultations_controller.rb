49e83594934acc28ed21dcd6f79cff72a65ccbee
Mauricio Homma <mhhomma@gmail.com>
2019-02-26 17:53:45 -0300

    Solved border problem

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -18,17 +18,18 @@ class ConsultationsController < ApplicationController
 
   def create
     @consultations = Consultation.all.where("doctor_id = #{params[:doctor_id]}")
-
     @doctor = Doctor.find(params[:doctor_id])
     @consultation = Consultation.new(consultation_params)
     authorize @consultation
     empty = true
     if @consultation.start_time < @consultation.end_time
       @consultations.each do |marked_consultation|
-        if @consultation.start_time.between?(marked_consultation.start_time, marked_consultation.end_time)
+        if (@consultation.start_time == marked_consultation.start_time) && (@consultation.end_time == marked_consultation.end_time)
+          empty = false
+        elsif (@consultation.start_time > marked_consultation.start_time) && (@consultation.start_time < marked_consultation.start_time)
           empty = false
         else
-          if @consultation.end_time.between?(marked_consultation.start_time, marked_consultation.end_time)
+          if (@consultation.end_time > marked_consultation.start_time) && (@consultation.end_time < marked_consultation.end_time)
             empty = false
           else
             if (@consultation.start_time < marked_consultation.start_time) && (@consultation.end_time > marked_consultation.end_time)
