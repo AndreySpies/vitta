ec7fcab87dd085f7a801bba3c5317462787472f4
Mauricio Homma <mhhomma@gmail.com>
2019-02-28 10:57:59 -0300

    Added devise exception

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,4 +1,5 @@
 class DoctorsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
   def index
     @doctors = policy_scope(Doctor.near('Sapiranga'))
     @doctors = Doctor.global_search(params[:keywords]) if params[:keywords].present?
