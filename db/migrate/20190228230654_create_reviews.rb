1e58dbb419b8da0f2efce4479e5116f82ba9c67b
Andrey Spies <andreyspies@gmail.com>
2019-03-01 16:45:35 -0300

    Config review form

--- db/migrate/20190228230654_create_reviews.rb
+++ db/migrate/20190228230654_create_reviews.rb
@@ -1,7 +1,7 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
-      t.string :content
+      t.text :content
       t.integer :rating
       t.references :user, foreign_key: true
       t.references :doctor, foreign_key: true
