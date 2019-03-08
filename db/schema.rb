c855ed8a48fbef6d13df387c28ba0244dabdee2b
Kali Ebert <kalijke@hotmail.com>
2019-03-08 16:31:41 -0300

    add-photos-change-review-doctors-show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_08_130739) do
+ActiveRecord::Schema.define(version: 2019_03_08_124911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2019_03_08_130739) do
     t.datetime "updated_at", null: false
     t.integer "price_cents", default: 0, null: false
     t.string "price_currency", default: "BRL", null: false
-    t.datetime "start_time", default: "2019-03-08 14:12:26"
+    t.datetime "start_time", default: "2019-03-08 17:15:45"
     t.datetime "end_time"
     t.index ["doctor_id"], name: "index_consultations_on_doctor_id"
     t.index ["patient_id"], name: "index_consultations_on_patient_id"
@@ -70,9 +70,7 @@ ActiveRecord::Schema.define(version: 2019_03_08_130739) do
     t.string "recipient_id"
     t.integer "bank_account_id"
     t.string "status"
-    t.bigint "work_schedule_id"
     t.index ["user_id"], name: "index_doctors_on_user_id"
-    t.index ["work_schedule_id"], name: "index_doctors_on_work_schedule_id"
   end
 
   create_table "medical_records", force: :cascade do |t|
@@ -166,7 +164,6 @@ ActiveRecord::Schema.define(version: 2019_03_08_130739) do
   add_foreign_key "doctor_specialties", "doctors"
   add_foreign_key "doctor_specialties", "specialties"
   add_foreign_key "doctors", "users"
-  add_foreign_key "doctors", "work_schedules"
   add_foreign_key "medical_records", "users"
   add_foreign_key "orders", "consultations"
   add_foreign_key "orders", "doctors"
